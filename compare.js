38491dc40b3792bc4e14f0b2a94d6d7071abc517
ashfaq11 <ashfaqonly1@gmail.com>
2024-07-30 20:14:37 +0530

    Update compare.js

--- compare.js
+++ compare.js
@@ -1,9 +1,8 @@
 
 
 
-
-let file1="Flags1.yaml"
-let file2="Flags2.yaml"
+let file1="../plat/libs/java/sdk-fflags/testing-applications/openfeature-harness-json-yaml/Flags1.yaml"
+let file2="../plat/libs/java/sdk-fflags/testing-applications/openfeature-harness-json-yaml/Flags2.yaml"
 
 const yaml = require('yaml');
 const fs   = require('fs');
